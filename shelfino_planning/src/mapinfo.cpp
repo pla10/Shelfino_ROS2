b3327eb1369d4e152a1250176c66e4a3b9f6944f
Ruben <ruben.malacarne@gmail.com>
2024-12-10 16:50:05 +0100

    add subscription on map, gates and obstacles

--- shelfino_planning/src/mapinfo.cpp
+++ shelfino_planning/src/mapinfo.cpp
@@ -1,5 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
+
 #include "geometry_msgs/msg/polygon.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
+
+#include "obstacles_msgs/msg/obstacle_array_msg.hpp"
+#include "obstacles_msgs/msg/obstacle_msg.hpp"
 
 #include <iostream>
 #include <vector>
@@ -42,6 +47,16 @@ public:
             "map_borders",  
             qos,   
             std::bind(&MapInformationNode::get_map_boarder_callback, this, std::placeholders::_1));
+        
+        gate_subscription_ = this->create_subscription<geometry_msgs::msg::PoseArray>(
+            "gates",  
+            qos,   
+            std::bind(&MapInformationNode::get_gate_callback, this, std::placeholders::_1));
+        
+        obstacle_subscription_ = this->create_subscription<obstacles_msgs::msg::ObstacleArrayMsg>(
+            "obstacles",  
+            qos,   
+            std::bind(&MapInformationNode::get_obstacle_callback, this, std::placeholders::_1));
     }
 
 private:
@@ -53,9 +68,49 @@ private:
             RCLCPP_INFO(this->get_logger(), "  point %zu: x=%.2f, y=%.2f, z=%.2f", i + 1, point.x, point.y, point.z);
         }
     }
-    rclcpp::Subscription<geometry_msgs::msg::Polygon>::SharedPtr map_subscription_;
+    void get_gate_callback(const geometry_msgs::msg::PoseArray::SharedPtr msg){
+        RCLCPP_INFO(this->get_logger(), "Received PoseArray with frame_id: %s, stamp: %d.%09d",
+                msg->header.frame_id.c_str(),
+                msg->header.stamp.sec,
+                msg->header.stamp.nanosec);
 
-    
+        for (size_t i = 0; i < msg->poses.size(); ++i) {
+            const auto &pose = msg->poses[i];
+            RCLCPP_INFO(this->get_logger(), "Pose %zu: Position(x=%.f, y=%.f, z=%.f), Orientation(x=%.f, y=%.f, z=%.f, w=%.f)",
+                        i + 1,
+                        pose.position.x, pose.position.y, pose.position.z,
+                        pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w);
+        }
+    }
+    void get_obstacle_callback(const obstacles_msgs::msg::ObstacleArrayMsg::SharedPtr msg){
+        RCLCPP_INFO(this->get_logger(), "Obstecles recived with with frame_id: %s, stamp: %d.%09d",
+                msg->header.frame_id.c_str(),
+                msg->header.stamp.sec,
+                msg->header.stamp.nanosec);
+        
+        for (size_t i = 0; i < msg->obstacles.size(); ++i) {
+            const auto &obstacle = msg->obstacles[i];
+
+            RCLCPP_INFO(this->get_logger(), "Obstacle %zu: frame_id: %s, stamp: %d.%09d",
+                        i + 1,
+                        obstacle.header.frame_id.c_str(),
+                        obstacle.header.stamp.sec,
+                        obstacle.header.stamp.nanosec);
+
+            RCLCPP_INFO(this->get_logger(), "  Polygon:");
+            for (size_t j = 0; j < obstacle.polygon.points.size(); ++j) {
+                const auto &point = obstacle.polygon.points[j];
+                RCLCPP_INFO(this->get_logger(), "    Point %zu: x=%.2f, y=%.f, z=%.f",
+                            j + 1, point.x, point.y, point.z);
+            }
+
+            RCLCPP_INFO(this->get_logger(), "  Radius: %.f", obstacle.radius);
+        }
+    }
+
+    rclcpp::Subscription<geometry_msgs::msg::Polygon>::SharedPtr map_subscription_;
+    rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr gate_subscription_;
+    rclcpp::Subscription<obstacles_msgs::msg::ObstacleArrayMsg>::SharedPtr obstacle_subscription_;
 };
 
 
